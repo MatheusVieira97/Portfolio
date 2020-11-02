603f5b1f401d0e3764a0250c67d8dd05ff3c41aa
Matheus Vieira <matheus.soares.vieira@gmail.com>
2020-11-01 23:34:04 -0200

    Mobile version upload

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+//Menu
+const navSlide = () => {
+    const iconMenu = document.querySelector('.btn-Menu');
+    const nav = document.querySelector('.menu');
+
+    //Onclick do icon do menu
+    iconMenu.addEventListener('click', () => {
+        nav.classList.toggle('menu-active');
+    })
+}
+navSlide();
+
+//Dark Mode
+const darkMode = () => {
+    const html = document.querySelector('html');
+    const checkbox = document.querySelector('#darkMode');
+
+    checkbox.addEventListener('change', () => {
+        html.classList.toggle('dark');
+    })
+}
+darkMode();
