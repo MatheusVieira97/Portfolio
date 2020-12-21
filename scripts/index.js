b332a3b3cd9a0ba6f4d9927bdb80095636cda4f2
Matheus Vieira <matheus.msv1997@gmail.com>
2020-12-21 01:02:02 -0300

    Created version for 1920x1080

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,22 @@
 //Menu
 const navSlide = () => {
-    const iconMenu = document.querySelector('.btn-Menu');
-    const nav = document.querySelector('.menu');
+  const iconMenu = document.querySelector(".btn-Menu");
+  const nav = document.querySelector(".menu");
 
-    //Onclick do icon do menu
-    iconMenu.addEventListener('click', () => {
-        nav.classList.toggle('menu-active');
-    })
-}
+  //Onclick do icon do menu
+  iconMenu.addEventListener("click", () => {
+    nav.classList.toggle("menu-active");
+  });
+};
 navSlide();
 
 //Dark Mode
 const darkMode = () => {
-    const html = document.querySelector('html');
-    const checkbox = document.querySelector('#darkMode');
+  const html = document.querySelector("html");
+  const checkbox = document.querySelector("#darkMode");
 
-    checkbox.addEventListener('change', () => {
-        html.classList.toggle('dark');
-    })
-}
+  checkbox.addEventListener("change", () => {
+    html.classList.toggle("dark");
+  });
+};
 darkMode();
